e90bd69da8cbc6ba1e751396995ea13e52d4576c
fnikme <f.n@ik.me>
2025-01-07 17:13:54 +0100

    dynamiser

--- Route.js
+++ Route.js
@@ -1,8 +1,16 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize;
     }
 }
+/*
+[] -> Tout le monde peut y accéder
+["disconnected"] ->Réservé aux utilisaters déconnectés
+["client"] ->Réservé aux utilisaters avec le rôle client
+["admin"] ->Réservé aux utilisaters avec le rôle admin
+["admin", "client"] ->Réservé aux utilisaters avec le rôle admin OU client
+*/
